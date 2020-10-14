320098df620500704dda1dc657464b12563c5d39
rfkinline <robert@lightdec.com>
2020-10-14 08:28:12 -0400

    improving error handling

--- ETHDashboard.py
+++ ETHDashboard.py
@@ -6,9 +6,10 @@ import time
 import datetime
 from urllib.request import urlopen
 from json import loads
+
 # your defipulse API key. 
-defipulseApikey = "93c078480f89a7fa220f2b91a7244ea17b5bab77e3cff6b0fa1e2d0ed22c"
-#"e61b012ae1c05cd4f84bd87c86826ec28f2fde511db9e73fddf9a0a510d"
+defipulseApikey = "3c078480f89a7fa220f2b91a7244ea17b5bab77e3cff6b0fa1e2d0ed22c"
+
 #display tresholds (change color if x value increased more than y%). 
 price1hrchangediff = 1    # checked once / hr
 price24hrchangediff = 5    # checked once / hr
@@ -24,6 +25,7 @@ class ETHTicker:
 
 	def labels():
 		hwg()
+		internet_on()
 		title = "Market Data"
 		down_label = Label(text=(title),anchor=NW, justify=LEFT,font=('Helvetica', 28, 'bold'), bg='black', fg='gold')
 		down_label.grid(row=2, column=1, sticky=W)
@@ -67,64 +69,58 @@ class ETHTicker:
 		down_label = Label(text=(text14),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg=color)
 		down_label.grid(row=5, column=1, sticky=W)
 
-
-		currency = "{:,.2%}".format(priceeth24hrchange)
-		text2 = "24hr change: " + str(currency)
-		down_label = Label(text=(text2),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg = "white")
-		down_label.grid(row=6, column=1, sticky=W)
-
 		currency = "{:,.2%}".format(market_dominance_percentage)
 		text3 = "ETH Dominance: " + str(currency)
 		down_label = Label(text=(text3),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg = 'white')
-		down_label.grid(row=7, column=1, sticky=W)
+		down_label.grid(row=6, column=1, sticky=W)
 
 		currency = "{:,.0f}".format(marketcapeth)
-		text4 = "Marketcap: $" + str(currency)
+		text4 = "ETH Marketcap: $" + str(currency)
 		down_label = Label(text=(text4 + '\n'),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg = 'white')
-		down_label.grid(row=8, column=1, sticky=W)
+		down_label.grid(row=7, column=1, sticky=W)
 
 		title = "Blockchain Data"
 		down_label = Label(text=(title),anchor=NW, justify=LEFT,font=('Helvetica', 28, 'bold'), bg='black', fg='gold')
-		down_label.grid(row=9, column=1, sticky=W)
+		down_label.grid(row=8, column=1, sticky=W)
 
 		currency = "{:,.0f}".format(average_gasfee)
 		text5 = "Average Gas: " + str(currency) + " gwei  "
 		date_time_obj = "{:,.1f}".format(average_wait_time)
 		text5b = "Avg Wait time: " + str(date_time_obj) + " min  "
 		down_label = Label(text=(text5 + '\n' + text5b),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg='white')
-		down_label.grid(row=10, column=1, sticky=W)
+		down_label.grid(row=9, column=1, sticky=W)
 
 		date_time_obj =  "{:,.2f}".format(average_block_time)
 		text5a = "Avg Block time: " + str(date_time_obj) + " sec  "
 		down_label = Label(text=(text5a + '\n'),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg='white')
-		down_label.grid(row=11, column=1, sticky=W)
+		down_label.grid(row=10, column=1, sticky=W)
 
 		title = "DeFi"
 		down_label = Label(text=(title),anchor=NW, justify=LEFT,font=('Helvetica', 28, 'bold'), bg='black', fg='gold')
-		down_label.grid(row=12, column=1, sticky=W)
+		down_label.grid(row=11, column=1, sticky=W)
 
 		currency = "${:,.1f}".format(defilockedusd)
 		text10 = "Value locked: " + str(currency) + " B  "
 		currency = "${:,.1f}".format(dominance_valueusd)  + " B  "
 		text11 = str(dominance_name) + " locked: " + str(currency)
 		down_label = Label(text=(text10 + '\n' + text11),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg='white')
-		down_label.grid(row=13, column=1, sticky=W)
+		down_label.grid(row=12, column=1, sticky=W)
 
 		currency = "{:,.0f}".format(TVLBTC)
 		text12 = "BTC locked: " + str(currency) + u'\u20bf'
 		currency = "{:,.0f}".format(LNDBTC)
 		text12a = "Lightning volume: " + str(currency) + u'\u20bf'
 		down_label = Label(text=(text12 + '\n' + text12a + '\n'),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg='white')
-		down_label.grid(row=14, column=1, sticky=W)
+		down_label.grid(row=13, column=1, sticky=W)
 					
 		text98 = str(errormessage)
-		down_label = Label(text=(text98),anchor=NW, justify=LEFT,font=('Helvetica',12), bg='black', fg='red')
-		down_label.grid(row=17, column=1, sticky=W)
+		down_label = Label(text=(text98),anchor=NW, justify=LEFT,font=('Helvetica',14), bg='black', fg='red')
+		down_label.grid(row=16, column=1, sticky=W)
 	
 		now = datetime.datetime.now()
 		text99 = "Current time: " + str(now)
 		down_label = Label(text=(text99),anchor=NW, justify=LEFT,font=('Helvetica',12), bg='black', fg='white')
-		down_label.grid(row=18, column=1, sticky=W)
+		down_label.grid(row=17, column=1, sticky=W)
 	
 # This is where you set the update time. 290000 is about 5 minutes	
 		down_label.after(290000,ETHTicker.labels)
@@ -147,6 +143,7 @@ def hwg():
 	global defilockedusd
 	global dominance_name
 	global dominance_valueusd
+	global errormessage
 	global TVLBTC
 	global LNDBTC
 	global status
@@ -170,6 +167,7 @@ def hwg():
 		status = urltest.status_code
 		if status == 429:
 			print("Error DefiPulse. Wrong or expired API key")
+			errormessage = "Error DefiPulse. Wrong or expired API key"
 			raise
 		elif status == 200:
 			total_value_locked = requests.get(defi_pulse_url)
@@ -184,9 +182,10 @@ def hwg():
 				elif name == 'Lightning Network':
 					LNDBTC = project['value']['tvl']['BTC'].get("value")
 			TVLBTC = WBTC + RENBTC + LNDBTC
+		else:
+			print("Error reading DeFiPulse. Error-code: " + str(status))
+			errormessage = "Unknown error reading DeFiPulse Project"
 	except:
-		print("Error reading DeFiPulse. Error-code: " + str(status))
-		errormessage = "Error reading DeFiPulse Project"
 		if status == 204:
 			time.sleep(5)
 			hwg()
@@ -206,16 +205,21 @@ def hwg():
 			dominance_name = str(marketdata['All']['dominance_name'])
 			dominance_valueusd = dominance_valueusd / 1000000000
 			defilockedusd = defilockedusd / 1000000000
+		else:
+			print("Error reading DeFiPulse Market. Error-code: " + str(status))
+			errormessage = "Unknown error reading DeFiPulse Market"
 	except:
-		print("Error reading DeFiPulse. Error-code: " + str(status))
-		errormessage = "Error reading DeFiPulse Market"
 		if status == 204:
 			time.sleep(5)
 			hwg()
 
 	try:
 #	https://docs.ethgasstation.info/gas-price
-		marketdata = requests.get('https://ethgasstation.info/api/ethgasAPI.json?').json()
+		status = 0
+		ethgas_url = 'https://ethgasstation.info/api/ethgasAPI.json?'
+		urltest = requests.get(ethgas_url)
+		status = urltest.status_code
+		marketdata = requests.get(ethgas_url).json()
 		average_gasfee = marketdata['average']
 		average_block_time = marketdata['block_time']
 		average_wait_time = marketdata['avgWait']
@@ -223,19 +227,26 @@ def hwg():
 		average_gasfee = average_gasfee / 10
 
 	except:
-		print("Error reading Ethgasstation.info")
+		errormessage = "Error reading EthGasStation " + str(status)
+		print(errormessage)
 		time.sleep(10)
 		hwg()
-
+		
 	try:
 #	get blockchain data https://blockchair.com/api/docs#link_M03
-		blockchair_api_request = urlopen('https://api.blockchair.com/ethereum/stats').read()	
-		market_dominance_percentage = float(loads(blockchair_api_request)['data']['market_dominance_percentage'])
+		status = 0
+		blockchair_url = 'https://api.blockchair.com/ethereum/stats'
+		urltest = requests.get(blockchair_url)
+		status = urltest.status_code
+
+		blockchair_api_request = requests.get(blockchair_url).json()
+		market_dominance_percentage = blockchair_api_request['data']['market_dominance_percentage']
 
 		market_dominance_percentage = market_dominance_percentage / 100
 
 	except:
-		print("Error reading Blockchair")
+		errormessage = "Error reading Blockchair: " + str(status)
+		print(errormessage)
 		time.sleep(10)
 		hwg()
 	
@@ -267,6 +278,16 @@ def hwg():
 		print("Error reading Coingecko")
 		time.sleep(10)
 		hwg()
+
+def internet_on(url='http://www.google.com/', timeout=5):
+	try:
+		_ = requests.get(url, timeout=timeout)
+		return True
+	except requests.ConnectionError:
+		errormessage = "No internet connection available."
+	return False
+
+
 
 errormessage=""
 LNDBTC=0
