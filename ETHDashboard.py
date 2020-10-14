2ecd23ea7d0e32dd79f33189924685034cb1b3a9
rfkinline <robert@lightdec.com>
2020-10-14 17:14:38 -0400

    included new indicators

--- ETHDashboard.py
+++ ETHDashboard.py
@@ -70,51 +70,51 @@ class ETHTicker:
 
 		currency = "{:,.0f}".format(marketcapeth)
 		text4 = "ETH Marketcap: $" + str(currency)
-		down_label = Label(text=(text4 + '\n'),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg = 'white')
+		down_label = Label(text=(text4 + '\n' text4a),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg = 'white')
 		down_label.grid(row=7, column=1, sticky=W)
 
 		title = "Blockchain Data"
 		down_label = Label(text=(title),anchor=NW, justify=LEFT,font=('Helvetica', 28, 'bold'), bg='black', fg='gold')
-		down_label.grid(row=8, column=1, sticky=W)
+		down_label.grid(row=9, column=1, sticky=W)
 
 		currency = "{:,.0f}".format(average_gasfee)
 		text5 = "Average Gas: " + str(currency) + " gwei  "
 		date_time_obj = "{:,.1f}".format(average_wait_time)
 		text5b = "Avg Wait time: " + str(date_time_obj) + " min  "
 		down_label = Label(text=(text5 + '\n' + text5b),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg='white')
-		down_label.grid(row=9, column=1, sticky=W)
+		down_label.grid(row=10, column=1, sticky=W)
 
 		date_time_obj =  "{:,.2f}".format(average_block_time)
 		text5a = "Avg Block time: " + str(date_time_obj) + " sec  "
 		down_label = Label(text=(text5a + '\n'),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg='white')
-		down_label.grid(row=10, column=1, sticky=W)
+		down_label.grid(row=11, column=1, sticky=W)
 
 		title = "DeFi"
 		down_label = Label(text=(title),anchor=NW, justify=LEFT,font=('Helvetica', 28, 'bold'), bg='black', fg='gold')
-		down_label.grid(row=11, column=1, sticky=W)
+		down_label.grid(row=12, column=1, sticky=W)
 
 		currency = "${:,.1f}".format(defilockedusd)
 		text10 = "Value locked: " + str(currency) + " B  "
 		currency = "${:,.1f}".format(dominance_valueusd)  + " B  "
 		text11 = str(dominance_name) + " locked: " + str(currency)
 		down_label = Label(text=(text10 + '\n' + text11),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg='white')
-		down_label.grid(row=12, column=1, sticky=W)
+		down_label.grid(row=13, column=1, sticky=W)
 
 		currency = "{:,.0f}".format(TVLBTC)
 		text12 = "BTC locked: " + str(currency) + u'\u20bf'
 		currency = "{:,.0f}".format(LNDBTC)
 		text12a = "Lightning volume: " + str(currency) + u'\u20bf'
 		down_label = Label(text=(text12 + '\n' + text12a + '\n'),anchor=NW, justify=LEFT,font=('Helvetica',20), bg='black', fg='white')
-		down_label.grid(row=13, column=1, sticky=W)
+		down_label.grid(row=14, column=1, sticky=W)
 					
 		text98 = str(errormessage)
 		down_label = Label(text=(text98),anchor=NW, justify=LEFT,font=('Helvetica',14), bg='black', fg='red')
-		down_label.grid(row=16, column=1, sticky=W)
+		down_label.grid(row=17, column=1, sticky=W)
 	
 		now = datetime.datetime.now()
 		text99 = "Current time: " + str(now)
 		down_label = Label(text=(text99),anchor=NW, justify=LEFT,font=('Helvetica',12), bg='black', fg='white')
-		down_label.grid(row=17, column=1, sticky=W)
+		down_label.grid(row=18, column=1, sticky=W)
 	
 # This is where you set the update time. 290000 is about 5 minutes	
 		down_label.after(290000,ETHTicker.labels)
@@ -145,7 +145,9 @@ def hwg():
 	global average_wait_time
 	global priceyfi1hrchange
 	global priceuni1hrchange
+	global defi_market_cap
 
+	defi_market_cap = 0
 	priceeth = 0
 	priceeth1hrchange = 0
 	priceeth24hrchange = 0
@@ -270,6 +272,15 @@ def hwg():
 
 	except:
 		print("Error reading Coingecko")
+		time.sleep(10)
+		hwg()
+	
+	try:
+		coingecko_api_request = urlopen('https://api.coingecko.com/api/v3/global/decentralized_finance_defi').read()	
+		defi_market_cap = float(loads(coingecko_api_request)['data']['defi_market_cap'])
+
+	except:
+		print("Error reading Coingecko Defi")
 		time.sleep(10)
 		hwg()
 
